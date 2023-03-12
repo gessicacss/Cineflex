f83549668a53c01bdc56b3b3fda48d087ea103e0
Gessica <santos.gessicacarolina@gmail.com>
2023-03-12 14:39:32 -0300

    estilizando a mensagem de erro que aparece no campo cpf

--- src/pages/SeatsPage/componentsSeatsPage/Seats.js
+++ src/pages/SeatsPage/componentsSeatsPage/Seats.js
@@ -1,6 +1,15 @@
-export default function Seats({ selected, buyerInfo, handleSubmit, name, error }) {
+import styled from "styled-components";
+
+export default function Seats({
+  selected,
+  buyerInfo,
+  handleSubmit,
+  name,
+  error,
+}) {
   return (
     <div>
+      <Container>
       <label>
         Nome do Comprador {name}:
         <input
@@ -15,22 +24,35 @@ export default function Seats({ selected, buyerInfo, handleSubmit, name, error }
           placeholder="Digite seu nome..."
         />
       </label>
-
-      <label>
-        CPF do Comprador {name}:
-        <input
-          id="cpf"
-          data-test="client-cpf"
-          name={"cpf"}
-          required
-          type="number"
-          value={buyerInfo.cpf}
-          onChange={handleSubmit}
-          disabled={selected.length <= 0 ? true : false}
-          placeholder="Digite seu CPF..."
-        />
-        {error && <p>{error}</p>}
-      </label>
+      </Container>
+      <Container>
+        <label>
+          CPF do Comprador {name}:
+          <input
+            id="cpf"
+            data-test="client-cpf"
+            name={"cpf"}
+            required
+            type="number"
+            value={buyerInfo.cpf}
+            onChange={handleSubmit}
+            disabled={selected.length <= 0 ? true : false}
+            placeholder="Digite seu CPF..."
+          />
+          {error && <p>{error}</p>}
+        </label>
+      </Container>
     </div>
   );
 }
+
+const Container = styled.div`
+  position: relative;
+  p {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    font-size: 14px;
+    color: red;
+  }
+`;
